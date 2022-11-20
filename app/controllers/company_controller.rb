0bc13df03d94aeaa7fd81b95e1a1a2e28737d47e
tbzwolf2 <gabe.t.lovett@gmail.com>
2022-11-20 21:49:39 +0000

    CSS and some feature tweaks

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -6,12 +6,9 @@ class CompanyController < ApplicationController
         @company_json = JSON.parse(res.body)
 
         @company_json.each do |company|
-            puts company
             db_company = Company.find_or_create_by(api_id: company["number"])
-            puts "asdfasdfasdf"
-            puts db_company.inspect
             db_company.update(name: company["name"])
-            puts db_company.inspect
+            company["likes"] = db_company.likes
 
         end
     end
@@ -20,33 +17,22 @@ class CompanyController < ApplicationController
         if session[:liked_companies] == nil
             session[:liked_companies] = {}
         end
-        company = Company.find_by(api_id: params[:api_id])
+        @company = Company.find_by(api_id: params[:api_id])
 
         if params[:liked] == "true"
             session[:liked_companies][params[:api_id]] = DateTime.now
-            company.update(likes: (company.likes += 1))
+            @company.update(likes: (@company.likes += 1))
         end
+        session_time = DateTime.parse(session[:liked_companies][params[:api_id]].to_s) if session[:liked_companies][params[:api_id]]
 
-        if session[:liked_companies][params[:api_id]] and session[:liked_companies][params[:api_id]] < (DateTime.now + 1.hour)
+        if session[:liked_companies][params[:api_id]] and (session_time + 30.seconds) > DateTime.now
             @like_button_bool = false
         else
             @like_button_bool = true
         end
 
+        @company_json = JSON.parse(Company.get_gov_api("https://api.company-information.service.gov.uk/company/#{params[:api_id]}"))
 
-
-        uri = URI("https://api.company-information.service.gov.uk/company/#{params[:api_id]}")
-        req = Net::HTTP::Get.new(uri)
-        req.basic_auth '26f083e7-41fb-41b1-a20b-8b47555ad37f', ''
-
-        req_options = {
-          use_ssl: uri.scheme == "https"
-        }
-        res = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-            http.request(req)
-        end
-
-        @company_json = JSON.parse(res.body)
         @address = ""
         @company_json["registered_office_address"].each do |key, value|
             @address += value + ", "
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,17 @@
 class Company < ApplicationRecord
+
+  def self.get_gov_api(url)
+    uri = URI(url)
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth ENV['GOV_API_KEY'], ''
+
+    req_options = {
+      use_ssl: uri.scheme == "https"
+    }
+    res = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(req)
+    end
+
+    res.body
+  end
 end
